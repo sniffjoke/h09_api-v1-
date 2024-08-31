6bba3ea4ed03a5202ca7f279a3cedc5ced02c6d4
sniffjoke <sniffjoke@gmail.com>
2024-08-31 03:41:02 +0200

    fix for status 429

--- src/middlewares/rateLimitMiddleware.ts
+++ src/middlewares/rateLimitMiddleware.ts
@@ -9,12 +9,13 @@ import {ApiError} from "../exceptions/api.error";
 export const rateLimitMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
         let tryingCount: boolean
+        console.log(req.originalUrl)
         const rateLimitData: IRateLimit = {
-            URL: req.baseUrl,
+            URL: req.originalUrl,
             IP: ip.address(),
             date: new Date(Date.now()).toISOString()
         }
-        const rateIPEnters = await rateLimitCollection.find({IP: rateLimitData.IP}).toArray()
+        const rateIPEnters = await rateLimitCollection.find({IP: rateLimitData.IP, URL: rateLimitData.URL}).toArray()
         try {
             tryingCount = ((new Date(rateLimitData.date).getTime()) - (new Date(rateIPEnters[rateIPEnters.length - 5].date).getTime())) / 1000 < 10
             console.log(tryingCount)
